1b2d40faf83b3431df1d53b65622aabb11516976
Krishna <ram620155@gmail.com>
2024-08-03 12:06:28 +0530

    adding github link

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 var typed = new Typed('#element', {
-    strings: ['Frontend Developer', 'Graphic designer'],
+    strings: ['Frontend Developer'],
     typeSpeed: 150,
 });
+var nav = document.querySelector(".nav-links");
+var menuBtn = document.querySelector(".menu-icon");
+var toggelBtn = document.getElementById("toggel");
+var closeBtn = document.getElementById("close");
+
+menuBtn.addEventListener("click",()=>{
+    nav.style.top = '60px';
+    menuBtn.style.display = "none";
+    closeBtn.style.display = "block";
+})
+closeBtn.addEventListener("click",()=>{
+    nav.style.top = '-300px';
+    menuBtn.style.display = "block";
+    closeBtn.style.display = "none";
+})
